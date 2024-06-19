144167f46e5d313f19d83ab0642f1aa70939eafe
Anthony Lin <82299090+Recite1@users.noreply.github.com>
2024-06-19 00:32:54 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,11 +4,15 @@ import os
 
 app = Flask(__name__)
 
-
+database = os.getenv('DATABASE')
 
 @app.route('/')
 def index():
     return f'{{"message" : "Server is running"}}'
+    
+@app.route('/test')
+def test():
+    return f'{{"message" : {database}}}'
 
 
 if __name__ == '__main__':
